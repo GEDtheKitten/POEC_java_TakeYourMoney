35ddeffa8a75edef62004ad7c5978bba15fc08da
gg <gedthekitten@gmail.com>
2021-06-24 00:40:09 +0200

    evolution des scripts SQL

--- take_your_money_query.sql
+++ take_your_money_query.sql
@@ -5,13 +5,32 @@ USE take_your_money;
 -- rajouter un parametre de sortie pour tester le resultat 
 -- rajouter une pajination pour les resultats de sorties 
 
+-- trouver des rejex pour tester les entrees
+
 
 DROP PROCEDURE IF EXISTS P_ajouter_client;
 DROP PROCEDURE IF EXISTS P_ajouter_produit;
+DROP PROCEDURE IF EXISTS P_modifier_client;
+-- DROP PROCEDURE IF EXISTS P_modifier_produit;
 DROP PROCEDURE IF EXISTS P_supprimer_client;
 DROP PROCEDURE IF EXISTS P_supprimer_produit;
 DROP PROCEDURE IF EXISTS P_lister_client;
 DROP PROCEDURE IF EXISTS P_lister_produits;
+-- DROP PROCEDURE IF EXISTS P_produits_plus_vendus;
+-- DROP PROCEDURE IF EXISTS P_produits_moins_vendus;
+-- DROP PROCEDURE IF EXISTS P_produits_jamais_vendus;
+
+-- DROP PROCEDURE IF EXISTS P_CA_moyenne_mensuelle;
+-- DROP PROCEDURE IF EXISTS P_CA_moyenne_annuelle;
+-- DROP PROCEDURE IF EXISTS P_CA_par_mois;
+-- DROP PROCEDURE IF EXISTS P_CA_par_ans;
+-- DROP PROCEDURE IF EXISTS P_CA_total;
+
+-- DROP PROCEDURE IF EXISTS P_CA_moyenne_mensuelle_par_client;
+-- DROP PROCEDURE IF EXISTS P_CA_moyenne_annuelle_par_client;
+-- DROP PROCEDURE IF EXISTS P_CA_par_mois_par_client;
+-- DROP PROCEDURE IF EXISTS P_CA_par_ans_par_client;
+-- DROP PROCEDURE IF EXISTS P_CA_total_par_client;
 
 
 DELIMITER //
@@ -19,7 +38,7 @@ DELIMITER //
 
 -- Procedure - ajouter un client
 CREATE PROCEDURE P_ajouter_client (IN client_nom VARCHAR(50), IN client_prenom VARCHAR(50), IN client_adresse1 VARCHAR(50),
-								   IN client_adresse2 VARCHAR(50), IN client_cp VARCHAR(5), IN client_ville VARCHAR(30),IN client_anonymise BOOLEAN)
+								   IN client_adresse2 VARCHAR(50), IN client_cp VARCHAR(5), IN client_ville VARCHAR(30), IN client_anonymise BOOLEAN)
 BEGIN
     INSERT INTO Clients(client_nom, client_prenom, client_adresse1, client_adresse2, client_cp, client_ville, client_anonymise)
 	VALUES (client_nom, client_prenom, client_adresse1, client_adresse2, client_cp, client_ville, client_anonymise);
@@ -27,9 +46,21 @@ END //
 
 
 -- Procedure - ajouter un produit
-CREATE PROCEDURE P_ajouter_produit (IN produit_designation VARCHAR(50), IN produit_desactive BOOLEAN)
+CREATE PROCEDURE P_ajouter_produit (IN produit_designation VARCHAR(50), IN produit_desactive BOOLEAN, 
+									IN prix_date DATE, prix_HT DECIMAL(15,2))
 BEGIN
     INSERT INTO Produits(produit_designation, produit_desactive) VALUES (produit_designation, produit_desactive);
+    INSERT INTO Prix (prix_date, prix_HT, produit_ID) VALUES (prix_date, prix_HT, LAST_INSERT_ID());
+END //
+
+-- Procedure - modifier un client
+CREATE PROCEDURE P_modifier_client (IN client_ID INT, IN client_nom VARCHAR(50), IN client_prenom VARCHAR(50), IN client_adresse1 VARCHAR(50),
+								    IN client_adresse2 VARCHAR(50), IN client_cp VARCHAR(5), IN client_ville VARCHAR(30), IN client_anonymise BOOLEAN)
+BEGIN
+    UPDATE Clients
+	SET client_nom = client_nom, client_prenom = client_prenom, client_adresse1 = client_adresse1, client_adresse2 = client_adresse2,
+    client_cp = client_cp, client_ville = client_ville, client_anonymise = client_anonymise
+    WHERE client_ID = client_ID;
 END //
 
 
@@ -58,8 +89,9 @@ END //
 -- Procedure - lister les produits 
 CREATE PROCEDURE P_lister_produits ()
 BEGIN
-	SELECT produit_designation AS Produits
-	FROM Produits;
+	SELECT produit_designation, prix_HT, prix_date 
+	FROM Produits AS p
+    JOIN Prix AS px on p.produit_ID = px.produit_ID;
 END //
 
 
--- take_your_money_test_procedure.sql
+++ take_your_money_test_procedure.sql
@@ -6,13 +6,13 @@ SET SQL_SAFE_UPDATES=0;
 CALL P_ajouter_client ('FREYA', 'Le chat', 'rue du chat noir', 'Miaou', 33000, 'Bordeaux', false);
 
 -- test de la procedure P_ajouter_produit
-CALL P_ajouter_produit ('le carton magique', false);
+CALL P_ajouter_produit ('le carton magique', false, STR_TO_DATE('1/04/2018', '%d/%m/%Y'), 55.9);
 
 -- test de la procedure P_supprimer_client
-CALL P_supprimer_client ('FREYA', 'Le chat');
+-- CALL P_supprimer_client ('FREYA', 'Le chat');
 
 -- test de la procedure P_supprimer_produit
-CALL P_supprimer_produit ('le carton magique');
+-- CALL P_supprimer_produit ('le carton magique');
 
 -- test de la procedure P_lister_client
 CALL P_lister_client ();
