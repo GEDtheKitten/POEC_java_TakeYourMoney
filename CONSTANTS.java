e8afe60e633a024ff4bc2ef8e434b7fd9982a5d2
pascal-roux-cyber <85712408+pascal-roux-cyber@users.noreply.github.com>
2021-06-24 21:57:49 +0000

    Add files via upload

---
+++ CONSTANTS.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package takeyourmoney;
+
+/**
+ *
+ * @author Pascal
+ */
+
+// Cette classe recense des constantes pour tout le code
+public class CONSTANTS {
+    
+    // Connexion à la base de données
+    public static final String USER = "root";
+    public static final String PASS = "";   
+    //public static final String NAMEBDD = "javabdd";
+    public static final String NAMEBDD = "take_your_money";
+
+    // adresse de connexion et driver
+    //public static final String NAMEDRIVER = "mysql-connector-java.Driver";   // permuter les commentaires
+    public static final String NAMEDRIVER = "org.mariadb.jdbc.Driver";
+    public static final String ADRESSE = "jdbc:mysql://localhost:3306/" + NAMEBDD;
+    
+ 
+}
