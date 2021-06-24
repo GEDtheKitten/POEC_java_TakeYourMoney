a7feaa2bf4ed02c32c5a1d7100cdf3366d51b134
MarylorMY <85668943+MarylorMY@users.noreply.github.com>
2021-06-24 16:00:10 +0200

    Add files via upload
    Ajout des JDialog

---
+++ JPanelWithBackground.java
@@ -0,0 +1,29 @@
+package ProjetTakeYourMoney;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class JPanelWithBackground extends JPanel {
+
+	private Image backgroundImage;
+
+	public JPanelWithBackground() throws IOException {
+		backgroundImage = ImageIO.read(new File("banniereTYM.jpg"));
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+
+		// Draw the background image.
+		g.drawImage(backgroundImage, 0, 0, this);
+
+		g.setClip(0, 0, 800, 600);
+
+	}
+}
