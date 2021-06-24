d94e274ed42a17889b4f0cd95398109d6108cd47
pascal-roux-cyber <85712408+pascal-roux-cyber@users.noreply.github.com>
2021-06-24 21:58:32 +0000

    Add files via upload

---
+++ MainTakeYourMoney.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package takeyourmoney;
+
+import java.awt.*;
+import java.util.*;
+import javax.swing.*;
+
+/**
+ *
+ * @author Pascal
+ */
+public class MainTakeYourMoney {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {                    
+        FrameIHM ihm = new FrameIHM();        
+    }    
+}
