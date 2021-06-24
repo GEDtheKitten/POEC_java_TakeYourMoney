53e327f0f2146da4b744e4d20008c9b0522e46cb
gg <gedthekitten@gmail.com>
2021-06-24 11:38:20 +0200

    evolution des scripts SQL

--- take_your_money_query.sql
+++ take_your_money_query.sql
@@ -7,15 +7,18 @@ USE take_your_money;
 
 -- trouver des rejex pour tester les entrees
 
-
+DROP PROCEDURE IF EXISTS P_lister_client;
+DROP PROCEDURE IF EXISTS P_lister_produits;
+DROP PROCEDURE IF EXISTS P_get_client;
+DROP PROCEDURE IF EXISTS P_get_produit;
 DROP PROCEDURE IF EXISTS P_ajouter_client;
 DROP PROCEDURE IF EXISTS P_ajouter_produit;
 DROP PROCEDURE IF EXISTS P_modifier_client;
--- DROP PROCEDURE IF EXISTS P_modifier_produit;
+DROP PROCEDURE IF EXISTS P_modifier_produit;
+DROP PROCEDURE IF EXISTS P_modifier_prix_produit;
 DROP PROCEDURE IF EXISTS P_supprimer_client;
 DROP PROCEDURE IF EXISTS P_supprimer_produit;
-DROP PROCEDURE IF EXISTS P_lister_client;
-DROP PROCEDURE IF EXISTS P_lister_produits;
+
 -- DROP PROCEDURE IF EXISTS P_produits_plus_vendus;
 -- DROP PROCEDURE IF EXISTS P_produits_moins_vendus;
 -- DROP PROCEDURE IF EXISTS P_produits_jamais_vendus;
@@ -35,12 +38,47 @@ DROP PROCEDURE IF EXISTS P_lister_produits;
 
 DELIMITER //
 
+-- Procedure - lister les clients 
+CREATE PROCEDURE P_lister_client ()
+BEGIN
+	SELECT CONCAT(client_nom, ' ', client_prenom) AS Clients
+	FROM Clients;
+END //
+
+
+-- Procedure - lister les produits 
+CREATE PROCEDURE P_lister_produits ()
+BEGIN
+	SELECT produit_designation, prix_HT, prix_date 
+	FROM Produits AS p
+    JOIN Prix AS px on p.produit_ID = px.produit_ID;
+END //
+
+
+-- Procedure - selectionner un client par nom / prenom 
+CREATE PROCEDURE P_get_client (IN client_nom VARCHAR(50), IN client_prenom VARCHAR(50))
+BEGIN
+	SELECT * 
+	FROM Clients
+    WHERE client_nom = client_nom AND client_prenom = client_prenom;
+END //
+
+
+-- Procedure - selectionner un produits par designation
+CREATE PROCEDURE P_get_produit (IN produit_designation VARCHAR(50))
+BEGIN
+	SELECT * 
+	FROM Produits AS p
+    JOIN Prix AS px on p.produit_ID = px.produit_ID
+    WHERE p.produit_designation = p.produit_designation;
+END //
+
 
 -- Procedure - ajouter un client
 CREATE PROCEDURE P_ajouter_client (IN client_nom VARCHAR(50), IN client_prenom VARCHAR(50), IN client_adresse1 VARCHAR(50),
 								   IN client_adresse2 VARCHAR(50), IN client_cp VARCHAR(5), IN client_ville VARCHAR(30), IN client_anonymise BOOLEAN)
 BEGIN
-    INSERT INTO Clients(client_nom, client_prenom, client_adresse1, client_adresse2, client_cp, client_ville, client_anonymise)
+    INSERT INTO Clients (client_nom, client_prenom, client_adresse1, client_adresse2, client_cp, client_ville, client_anonymise)
 	VALUES (client_nom, client_prenom, client_adresse1, client_adresse2, client_cp, client_ville, client_anonymise);
 END //
 
@@ -49,7 +87,7 @@ END //
 CREATE PROCEDURE P_ajouter_produit (IN produit_designation VARCHAR(50), IN produit_desactive BOOLEAN, 
 									IN prix_date DATE, prix_HT DECIMAL(15,2))
 BEGIN
-    INSERT INTO Produits(produit_designation, produit_desactive) VALUES (produit_designation, produit_desactive);
+    INSERT INTO Produits (produit_designation, produit_desactive) VALUES (produit_designation, produit_desactive);
     INSERT INTO Prix (prix_date, prix_HT, produit_ID) VALUES (prix_date, prix_HT, LAST_INSERT_ID());
 END //
 
@@ -61,6 +99,32 @@ BEGIN
 	SET client_nom = client_nom, client_prenom = client_prenom, client_adresse1 = client_adresse1, client_adresse2 = client_adresse2,
     client_cp = client_cp, client_ville = client_ville, client_anonymise = client_anonymise
     WHERE client_ID = client_ID;
+END //
+
+
+-- Procedure - modifier un produit
+CREATE PROCEDURE P_modifier_produit (IN produit_designation VARCHAR(50), IN produit_desactive BOOLEAN, 
+									IN prix_date DATE, prix_HT DECIMAL(15,2))
+BEGIN
+    UPDATE Produits
+	SET produit_designation = produit_designation, produit_desactive = produit_desactive
+    WHERE produit_ID = produit_ID;
+    UPDATE Prix
+	SET prix_date = prix_date, prix_HT = prix_HT
+    WHERE produit_ID = produit_ID;
+END //
+
+
+-- Procedure - modifier le prix d'un produit
+CREATE PROCEDURE P_modifier_prix_produit (IN produit_designation VARCHAR(50), IN produit_desactive BOOLEAN, 
+									IN prix_date DATE, prix_HT DECIMAL(15,2))
+BEGIN
+    UPDATE Produits
+	SET produit_designation = produit_designation, produit_desactive = produit_desactive
+    WHERE produit_ID = produit_ID;
+    UPDATE Prix
+	SET prix_date = prix_date, prix_HT = prix_HT
+    WHERE produit_ID = produit_ID;
 END //
 
 
@@ -78,21 +142,7 @@ BEGIN
 END //
 
 
--- Procedure - lister les clients 
-CREATE PROCEDURE P_lister_client ()
-BEGIN
-	SELECT CONCAT(client_nom, ' ', client_prenom) AS Clients
-	FROM Clients;
-END //
 
-
--- Procedure - lister les produits 
-CREATE PROCEDURE P_lister_produits ()
-BEGIN
-	SELECT produit_designation, prix_HT, prix_date 
-	FROM Produits AS p
-    JOIN Prix AS px on p.produit_ID = px.produit_ID;
-END //
 
 
 DELIMITER ;
